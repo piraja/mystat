a9646fc5cfe5233d65d9b2ad6f52cc55883eac1b
Ole Martin <post@piraja.no>
2011-11-24 15:26:59 +0100

    ..

--- urls.py
+++ urls.py
@@ -12,5 +12,5 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # url(r'^admin/', include(admin.site.urls)),
+url(r'^admin/', include(admin.site.urls)),
 )
